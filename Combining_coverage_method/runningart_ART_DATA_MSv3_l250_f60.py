f13fe1e76ce188eda14196cf42881c2ebba04500
sond468 <37023160+sond468@users.noreply.github.com>
2019-03-15 12:04:51 -0400

    Add files via upload

---
+++ Combining_coverage_method/runningart_ART_DATA_MSv3_l250_f60.py
@@ -0,0 +1,13 @@
+import os
+file_name = input('File name ')
+files=[]
+
+with open(file_name+'.txt','r') as f:
+	for line in f:
+		files.append(line.strip())
+
+for i in range(len(files)):
+	filein = files[i]
+	outnum = filein[4:13]
+	outing = "art_illumina -ss MSv3 -i " + filein+ " -l 250 -f 60 -o "+ outnum
+	os.system(outing)
